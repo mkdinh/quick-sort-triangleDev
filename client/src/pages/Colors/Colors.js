1f3fee5e2519161527dd4abd69bc3514aa0830c7
Michael Dinh <mkdinh94@gmail.com>
2018-05-19 18:21:38 -0400

    working partioning logic for ccolor sort

--- client/src/pages/Colors/Colors.js
+++ client/src/pages/Colors/Colors.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { css, StyleSheet } from "aphrodite";
 import { Segment, Button, Header, Form } from "semantic-ui-react";
 import Section from "../../components/Section";
-import plotly from 'plotly.js/dist/plotly';
+import plotly from "plotly.js/dist/plotly";
 import { ColorList } from "./quicksort";
 import { ColorsSortForm } from "../../components/Form";
 
 export default class BasicQS extends Component {
   state = {
-    colors: new ColorList(0, 250, null, 50)
+    colors: new ColorList(0, 250, null, 50),
+    initColors: new ColorList(0, 250, null, 50),
   };
 
   size = 4;
@@ -18,103 +19,129 @@ export default class BasicQS extends Component {
   }
 
   handleSortColors = () => {
-    let colors;
-    console.log(this.state.colors)
-    
-    colors = this.state.colors.quickSortCoordinates(null, this.updateColors, null, null, "x");
-    colors = this.state.colors.quickSortCoordinates(null, this.updateColors, null, null, "y");
-    colors = this.state.colors.quickSortCoordinates(null, this.updateColors, null, null, "z");
+    let rgb;
 
-    this.setState({ colors }, this.updatePlot);
-  }
+    rgb = this.state.colors.quickSortCoordinates(null, null, null, "x");
+    // console.log("-----------------------------------------------");
+    rgb.quickSortCoordinates(null, null, null, "y");
+    rgb = rgb.quickSortCoordinates(rgb.colors, null, null, "z");
+
+    let colors = rgb.colors;
+    let init = this.state.initColors.colors;
+    let len = colors.length;
+
+    // for (let i = 0; i < len; i++) {
+    //   // console.log(
+    //   //   `sorted: ${colors[i].r} ${colors[i].g} ${colors[i].b} | init: ${
+    //   //     init[i].r
+    //   //   } ${init[i].g} ${init[i].b}`,
+    //   // );
+    //   console.log(
+    //     `sorted: ${colors[i].x} ${colors[i].y} ${colors[i].z} | init: ${
+    //       init[i].x
+    //     } ${init[i].y} ${init[i].z}`,
+    //   );
+    // }
+
+    this.setState({ colors: rgb }, this.updatePlot);
+  };
 
   handleSortColorsLive = () => {
-    this.state.colors.quickSortCoordinatesLive(null, this.updateColors)
-    .then(() => console.log('success!'))
+    this.state.colors
+      .quickSortCoordinatesLive(null, this.updateColors)
+      .then(() => console.log("success!"));
+  };
 
-  }
-
-  updateColors = (colors) => this.setState({ colors }, this.updatePlot);
+  updateColors = colors => this.setState({ colors }, this.updatePlot);
 
   handleGenColors = increment => {
     let colors = new ColorList(0, 250, null, increment);
     this.setState({ colors }, this.updatePlot);
-  }
+  };
 
   handleRandomColors = () => {
     let randomized = this.state.colors.RandomizeCoordinates();
     this.setState({ colors: randomized }, this.updatePlot);
-    
-  }
+  };
 
   renderPlot = () => {
     let colors = this.state.colors;
-    
-    plotly.newPlot("graphDiv", [{
-      type: "scatter3d",
-      mode: "markers",
-      x: colors.X,
-      y: colors.Y,
-      z: colors.Z,
-      name: "colors",
-      marker: {
-        color: colors.Code,
-        size: this.size
-      }
-    }], {
-      width: 1000,
-      height: 600
-    })
-  }
+
+    plotly.newPlot(
+      "graphDiv",
+      [
+        {
+          type: "scatter3d",
+          mode: "markers",
+          x: colors.X,
+          y: colors.Y,
+          z: colors.Z,
+          name: "colors",
+          marker: {
+            color: colors.Code,
+            size: this.size,
+          },
+        },
+      ],
+      {
+        width: 1000,
+        height: 600,
+      },
+    );
+  };
 
   updatePlot = () => {
     let colors = this.state.colors;
 
-    plotly.animate('graphDiv', {
-      data: [{
-        x: colors.X,
-        y: colors.Y,
-        z: colors.Z,
-        marker: {
-          color: colors.Code,
-          size: this.size
-        }
-      }]
-    }, {
+    plotly.animate(
+      "graphDiv",
+      {
+        data: [
+          {
+            x: colors.X,
+            y: colors.Y,
+            z: colors.Z,
+            marker: {
+              color: colors.Code,
+              size: this.size,
+            },
+          },
+        ],
+      },
+      {
         transition: {
-          duration: 0
+          duration: 0,
         },
         frame: {
           duration: 0,
-          redraw: false
-        }
-      });
+          redraw: false,
+        },
+      },
+    );
   };
 
   render() {
-
     return (
       <Section header={`RGB Colors Sorting`}>
-      <Section.Content hideOverflow>
-        <div className={css(styles.graphDiv)} id='graphDiv'>
-        </div>
-      </Section.Content>
+        <Section.Content hideOverflow>
+          <div className={css(styles.graphDiv)} id="graphDiv" />
+        </Section.Content>
 
-      <Section.Footer>
-        <ColorsSortForm
-          handleSortColors={this.handleSortColors}
-          handleSortColorsLive={this.handleSortColorsLive}
-          handleGenColors={this.handleGenColors}
-          handleRandomColors={this.handleRandomColors}
-        />
-      </Section.Footer>
-    </Section>
+        <Section.Footer>
+          <ColorsSortForm
+            handleSortColors={this.handleSortColors}
+            handleSortColorsLive={this.handleSortColorsLive}
+            handleGenColors={this.handleGenColors}
+            handleRandomColors={this.handleRandomColors}
+          />
+        </Section.Footer>
+      </Section>
     );
   }
 }
 
 const styles = StyleSheet.create({
   graphDiv: {
-    margin: "-10rem -4rem"
-  }
+    margin: "-10rem -4rem",
+  },
 });
--- client/src/pages/Colors/quicksort.js
+++ client/src/pages/Colors/quicksort.js
@@ -19,61 +19,65 @@ function ColorList(min, max, colors, increment) {
           color.x = r;
           color.y = g;
           color.z = b;
-          color.code = `rgb(${r},${g},${b})`
-          array.push(color); 
+          color.code = `rgb(${r},${g},${b})`;
+          array.push(color);
         }
       }
     }
-  
-    return array;
-  }
 
-  // generate an ordered list of colors to be used 
-  this.colors = colors ? colors : this.genColors(min, max);
+    return array;
+  };
+
+  this.deepClone = function(array) {
+    return [...array].map(el => ({ ...el }));
+  };
+
+  // generate an ordered list of colors to be used
+  this.colors = colors ? this.deepClone(colors) : this.genColors(min, max);
 
   // unpack each object in an array based on key
   // use with plotly 3d scatter plot
   this.unpack = function(key) {
-    let temp = [...this.colors];
+    let temp = [...this.colors].map(el => ({ ...el }));
     return temp.map(el => el[key]);
-  }
+  };
 
   // values for plotly scatter 3d;
-  this.R = this.unpack('r');
+  this.R = this.unpack("r");
 
-  this.G = this.unpack('g');
+  this.G = this.unpack("g");
 
-  this.B = this.unpack('b');
+  this.B = this.unpack("b");
 
-  this.X = this.unpack('x');
+  this.X = this.unpack("x");
 
-  this.Y = this.unpack('y');
+  this.Y = this.unpack("y");
 
-  this.Z = this.unpack('z');
+  this.Z = this.unpack("z");
 
-  this.Code = this.unpack('code');
+  this.Code = this.unpack("code");
 
   this.randomizeElements = function() {
     this.colors = this.colors.sort(el => Math.random() < 0.5);
-    this.R = this.unpack('r');
-    this.G = this.unpack('g');
-    this.B = this.unpack('b');
-    this.Code = this.unpack('code');
+    this.R = this.unpack("r");
+    this.G = this.unpack("g");
+    this.B = this.unpack("b");
+    this.Code = this.unpack("code");
   };
 
   this.RandomizeElements = function() {
-    let temp = [...this.colors].map(el => ({...el}));
+    let temp = [...this.colors].map(el => ({ ...el }));
     temp = temp.sort(el => Math.random() < 0.5);
     let newList = new ColorList(min, max, temp);
-    
+
     return newList;
   };
 
   this.RandomizeCoordinates = function() {
     // temp array referencing color list
-    let temp = [...this.colors].map(el => ({...el}));
+    let temp = [...this.colors].map(el => ({ ...el }));
     // randomized color list
-    let randomized = [...this.colors].map(el => ({...el}));
+    let randomized = [...this.colors].map(el => ({ ...el }));
     randomized.sort(el => Math.random() < 0.5);
     let len = temp.length;
     // loop through and swap xyz of temp array with randomized array
@@ -83,71 +87,83 @@ function ColorList(min, max, colors, increment) {
       temp[i].y = randomized[i].y;
       temp[i].z = randomized[i].z;
     }
-    return new ColorList(min, max, temp);
-  }
+    let newColorList = new ColorList(min, max, temp);
+    return newColorList;
+  };
 
-  this.quickSortCoordinates = function(array, update, low, high, coord) {
-    // return new Promise(resolve => {
-    //     setTimeout(() => {
-          // define default variables
-          // let coordinates = ["x", "y", "z"];
-          if (!array) array = this.colors;
-          if (!low) low = 0;
-          if (!high) high = array.length - 1;
+  this.quickSortCoordinates = function(array, low, high, coord) {
+    if (!array) {
+      array = this.colors;
+    }
 
-          // update current colors
-          // update(new ColorList(min, max, array));
+    if (!low) low = 0;
+    if (!high) high = array.length - 1;
 
-          // recursively sort through color list by x, y, z
-            if (array.length > 1) {
-              // find next pivot point
-              let index = this.partition(array, low, high, coord);
-              if (low < index - 1) {
-                this.quickSortCoordinates(array, update, low, index - 1, coord);
-              } 
+    // recursively sort through color list by x, y, z
+    if (array.length > 1) {
+      // find next pivot point
+      let index = this.partition(array, low, high, coord);
 
-              if(index + 1 < high) {
-                this.quickSortCoordinates(array, update, index + 1, high, coord);
-              }
-            }
-          // resolve();
-        // }, 0)
-        return (new ColorList(min, max, array));
-    // });
-  }
+      // if (coord === "y") {
+      //   console.log(array);
+      //   return;
+      // }
+
+      if (low < index - 1) {
+        this.quickSortCoordinates(array, low, index - 1, coord);
+      }
+
+      if (index + 1 < high) {
+        this.quickSortCoordinates(array, index + 1, high, coord);
+      }
+    }
+    return new ColorList(min, max, array, increment);
+  };
 
   this.quickSortCoordinatesLive = function(array, update, low, high, coord) {
     return new Promise(resolve => {
-        setTimeout(() => {
-          // define default variables
-          let coordinates = ["x", "y", "z"];
-          if (!array) array = this.colors;
-          if (!low) low = 0;
-          if (!high) high = array.length - 1;
+      setTimeout(() => {
+        // define default variables
+        let coordinates = ["x", "y", "z"];
+        if (!array) array = this.colors;
+        if (!low) low = 0;
+        if (!high) high = array.length - 1;
 
-          // update current colors
-          update(new ColorList(min, max, array));
-          
-          for(let i = 0; i < coordinates.length; i++) {
-            // recursively sort through color list by x, y, z
-            if (array.length > 1) {
-              // find next pivot point
-              let index = this.partition(array, low, high, coordinates[i]);
-              if (low < index - 1) {
-                resolve(this.quickSortCoordinatesLive(array, update, low, index - 1, coordinates[i]))
-              } 
-              
-              if(index + 1 < high) {
-                resolve(this.quickSortCoordinatesLive(array, update, index + 1, high, coordinates[i]))
-              }
+        // update current colors
+        update(new ColorList(min, max, array));
+
+        for (let i = 0; i < coordinates.length; i++) {
+          // recursively sort through color list by x, y, z
+          if (array.length > 1) {
+            // find next pivot point
+            let index = this.partition(array, low, high, coordinates[i]);
+            if (low < index - 1) {
+              this.quickSortCoordinatesLive(
+                array,
+                update,
+                low,
+                index - 1,
+                coordinates[i],
+              );
+            }
+
+            if (index + 1 < high) {
+              this.quickSortCoordinatesLive(
+                array,
+                update,
+                index + 1,
+                high,
+                coordinates[i],
+              );
             }
           }
-          resolve();
-        }, 0)
+        }
+        resolve();
+      }, 0);
     });
-  }
+  };
 
-  this.partition = function (array, low, high, key) {
+  this.partition = function(array, low, high, key) {
     // define pivot point for comparsion
     let pivot = array[high];
     // swap index
@@ -156,33 +172,46 @@ function ColorList(min, max, colors, increment) {
     // loop through each element
     // compare key-value and swap index with wall index if less than pivot
     for (let i = low; i < high; i++) {
-      if (array[i][key] < pivot[key]) {
+      if (array[i].x < pivot.x) {
+        swap(array, i, wall);
+        wall++;
+      }
+
+      if (array[i].x === pivot.x && array[i].y < pivot.y) {
         swap(array, i, wall, key);
         wall++;
       }
-    } 
+      if (
+        array[i].x === pivot.x &&
+        array[i].y === pivot.y &&
+        array[i].z < pivot.z
+      ) {
+        swap(array, i, wall, key);
+        wall++;
+      }
+    }
+
     // swap pivot with wall position
     // everything to the left of pivot should be smaller
     // everything to the right should be larger
-    swap(array, wall, high, key);
-
+    swap(array, wall, high);
     return wall;
-  }
+  };
 
-  function swap (array, a, b, key) {
-      let temp = { ...array[a] };
-      // array[a][key] = array[b][key];
-      // array[b][key] = temp[key];
+  function swap(array, a, b, key) {
+    let tempArray = [...array];
+    let temp = { ...tempArray[a] };
+    // array[a][key] = array[b][key];
+    // array[b][key] = temp[key];
 
-      array[a].x = array[b].x;
-      array[a].y = array[b].y;
-      array[a].z = array[b].z;
+    array[a].x = array[b].x;
+    array[a].y = array[b].y;
+    array[a].z = array[b].z;
 
-      array[b].x = temp.x;
-      array[b].y = temp.y;
-      array[b].z = temp.z;
+    array[b].x = temp.x;
+    array[b].y = temp.y;
+    array[b].z = temp.z;
   }
 }
 
-
-export { ColorList }
+export { ColorList };
